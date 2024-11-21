1882f115e394d92ed81f91dc18cfc9cc600192b9
skye <1429420771@qq.com>
2024-11-21 18:15:12 +0800

    更新

--- src/main/java/com/ruoyi/project/analysis/controller/ProjectReportController.java
+++ src/main/java/com/ruoyi/project/analysis/controller/ProjectReportController.java
@@ -1,7 +1,9 @@
 package com.ruoyi.project.analysis.controller;
 
+import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.project.analysis.domain.ProjectDTO;
 import com.ruoyi.project.analysis.service.ProjectReportService;
+import com.sun.jna.platform.win32.WinBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.ruoyi.common.utils.SecurityUtils.hasRole;
 
 @RestController
 @RequestMapping("/reports")
@@ -24,16 +28,20 @@ public class ProjectReportController {
     /**
      * 根据用户ID获取所有项目及其详细信息
      *
-     * @param userId 租户ID
      * @return 项目详情列表
      */
-    @GetMapping("/projectInfo/{userId}")
-    public ResponseEntity<List<ProjectDTO>> getProjectsByTenantId(@PathVariable Long userId) {
+    @GetMapping("/projectInfo")
+    public ResponseEntity<List<ProjectDTO>> getProjectsByTenantId() {
         //加一个权限判断，调用不同方法
         try {
-            //List<ProjectDTO> projects = projectService.getProjectsByAccessorId(userId);
-            //List<ProjectDTO> projects = projectService.getProjectsByAuditorId(userId);
-            List<ProjectDTO> projects = projectService.getProjectsByTenantId(userId);
+            List<ProjectDTO> projects = new ArrayList<>();
+            if(hasRole("ROLE_AUDITOR")){
+                projects = projectService.getProjectsByAuditorId(SecurityUtils.getUserId());
+            }else if(hasRole("ROLE_ACCESSOR")){
+                projects = projectService.getProjectsByAccessorId(SecurityUtils.getUserId());
+            }else{
+                projects = projectService.getProjectsByTenantId(SecurityUtils.getUserId());
+            }
             return ResponseEntity.ok(projects);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(null);
